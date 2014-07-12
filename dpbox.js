b9da9e77ba3e232645ceb294af91f9853633957d
Wayne <dev.wayne.ix@gmail.com>
2014-07-12 18:53:58 +0800

    addopen

--- dpbox.js
+++ dpbox.js
@@ -1,28 +1,60 @@
 (function($) {
 
-	$.fn.dpbox = function(options) {	
-		options || (options = {});
-		return new dpbox(this,$.extend(options,$.fn.dpbox.defaults));
-	}
+	function dpbox(_appendTo, options) {
 
-	function dpbox(_appendTo,args){
-		this.wrapper = $('<div/>', {
-			'class': 'dpbox'
-		}).css({
-			position: 'absolute',
-			zIndex: '1000'
-		});
+		this.wrapper = $(_appendTo).addClass('dpbox').css('display','none');
+
+		this.title = $('<div/>', {
+			'class': 'dpbox-title'
+		}).appendTo(this.wrapper);
+
 		this.container = $('<div/>', {
 			'class': 'dpbox-container'
 		}).appendTo(this.wrapper);
+
 		this.content = $('<div/>', {
 			'class': 'dpbox-content'
-		}).appendTo(this.wrapper);
-		this.wrapper.appendTo(_appendTo);
+		}).css({
+			width: options.width,
+			height: options.height
+		}).appendTo(this.container);
+
+		this.setTitle(options.title);
+		this.setContent(options.content);
+		//this.show(this.wrapper);
+	}
+
+	dpbox.prototype.setContent = function(content) {
+		if (content == null && !this.wrapper) return;
+		this.content.html(content);
+	}
+
+	dpbox.prototype.setTitle = function(title) {
+		if (title == null && !this.wrapper) return;
+		this.title.html(title);
+	}
+
+
+	dpbox.prototype.open = function() {
+		var top = ($(window).height() - $(this.wrapper).height()) / 2;
+		var left = ($(window).width() - $(this.wrapper).width()) / 2;
+		var scrollTop = $(document).scrollTop();
+		var scrollLeft = $(document).scrollLeft();
+		$(this.wrapper).css({
+			position: 'absolute',
+			'top': top + scrollTop,
+			'left': left + scrollLeft
+		}).show();
+	}
+
+	$.fn.dpbox = function(options) {
+		options || (options = {});
+		return new dpbox(this, $.extend(options, $.fn.dpbox.defaults));
 	}
 
 	$.fn.dpbox.defaults = {
-
+		width: 'auto',
+		height: 'auto'
 	}
 
 })(jQuery)
