d250f18d3a86051f2a3331de733eb5d1ba894f97
Wayne <dev.wayne.ix@gmail.com>
2014-07-16 11:47:28 +0800

    addbtn

--- dpbox.js
+++ dpbox.js
@@ -5,17 +5,19 @@ function dpbox(type, options) {
 	this.defaults = {
 		width: 'auto',
 		height: 'auto',
-		overlay:'false',
+		overlay: 'false',
 		type: 'Modal'
 	}
 
-	this.opts = $.extend(this.opts, this.defaults,options);
+	this.opts = $.extend(this.opts, this.defaults, options);
 
 	this._create = function() {
 
 		if (this.container) return;
 
-		this.container = $('<div/>',{'class':'dpbox'}).css('display', 'none').appendTo($('body'));
+		this.container = $('<div/>', {
+			'class': 'dpbox'
+		}).css('display', 'none').appendTo($('body'));
 
 
 		this.header = $('<div/>', {
@@ -28,7 +30,7 @@ function dpbox(type, options) {
 
 		this.clzBtn = $('<div/>', {
 			'class': 'dpbox-clzBtn'
-		}).appendTo(this.header).html('X').css('display', 'inline').on('click',this.close.bind(this));
+		}).appendTo(this.header).html('X').css('display', 'inline').on('click', this.close.bind(this));
 
 		this.content = $('<div/>', {
 			'class': 'dpbox-content'
@@ -41,6 +43,12 @@ function dpbox(type, options) {
 		this.setTitle(this.opts.title);
 		this.setContent(this.opts.content);
 		//this.show(this.container);
+		if (this.opts.btn == "Confirm") {
+			this.bottom = $('<div/>', {
+				'class': 'dpbox-bottom'
+			}).appendTo(this.container);
+			this.setConfirmBtn();
+		}
 	};
 	//this._create();
 
@@ -58,11 +66,12 @@ dpbox.prototype.setTitle = function(title) {
 	if (title == null && !this.container) return;
 	this.title.html(title);
 }
+dpbox.prototype.setConfirmBtn = function() {
+	var confirmBtn = "<div class='buttons'><a href='' class='bn-yel bn-sm'>确定</a><a href='' class='bn-ash bn-sm'>取消</a></div>"
+	this.bottom.html(confirmBtn);
+}
 
-
-dpbox.prototype.open = function(options) {
-
-	var opts=this.opts;
+dpbox.prototype.open = function() {
 	var open = function() {
 		var top = ($(window).height() - $(this.container).height()) / 2;
 		var left = ($(window).width() - $(this.container).width()) / 2;
@@ -77,9 +86,9 @@ dpbox.prototype.open = function(options) {
 
 	if (!this.Opening) {
 		this.Opening = !this.Opening;
-		if(opts.overlay){
-			$('<div id="_overlay_" style="background-color: rgb(0, 0, 0); border-top-width: 1px; border-top-style: solid; border-top-color: rgb(0, 0, 0); position: absolute; height: 1557px;width: 100%; left: 0px; top: 0px; opacity: 0.7; tp-a"></div>').appendTo($('body'));
-
+		//Overlay
+		if (this.opts.overlay) {
+			this.overlay = $('<div style="background-color: rgb(0, 0, 0); border-top-width: 1px; border-top-style: solid; border-top-color: rgb(0, 0, 0); position: absolute; height: 1557px;width: 100%; left: 0px; top: 0px; opacity: 0.7; tp-a"></div>').appendTo($('body'));
 		}
 		open();
 	} else {
@@ -93,10 +102,16 @@ dpbox.prototype.close = function() {
 	};
 	if (this.Opening) {
 		this.Opening = !this.Opening;
+		if (this.opts.overlay) {
+			this.overlay.css({
+				display: 'none'
+			});
+		}
 		this.container.css({
 			display: 'none',
 			//opacity: 0
 		});
+
 	} else {
 		this.container.css({
 			display: 'none',
